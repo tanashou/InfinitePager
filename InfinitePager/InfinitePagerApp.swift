0e98c6cca8fc2abbf7907c93301230321bed426e
tanashou <tanashou0210@gmail.com>
2023-05-07 00:45:46 +0900

    Initial Commit

---
+++ InfinitePager/InfinitePagerApp.swift
@@ -0,0 +1,18 @@
+//
+//  InfinitePagerApp.swift
+//  InfinitePager
+//
+//  Created by tanashou on 2023/05/07
+//  
+//
+
+import SwiftUI
+
+@main
+struct InfinitePagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
